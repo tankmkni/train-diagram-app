5b44c36d859fd85868545f5fde0d5a1c8aea9989
chisasada <111257032+chisasada@users.noreply.github.com>
2026-01-10 15:20:33 +0900

    providers整理

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideRouter } from '@angular/router';
-import { AppRoutes } from './app/app.routes';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(AppRoutes),
-    provideCharts(withDefaultRegisterables()),
-    provideHttpClient()
-  ]
-});
+bootstrapApplication(App, appConfig);
